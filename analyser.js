76831c4f96ae155adfd33dc2eeb131793293c579
JackNUMBER <antoine.cdrt@gmail.com>
2023-08-02 01:52:21 +0200

    create output .geojson files on analyse

--- analyser.js
+++ analyser.js
@@ -7,6 +7,14 @@ const OPTIONS = { steps: 10, units: 'meters' };
 
 const inputScrapFilePath = path.join(__dirname, 'output/tesla_stores.geojson');
 const inputOverpassFilePath = path.join(__dirname, 'overpass-export.geojson');
+const outputInOsmFilePath = path.join(
+  __dirname,
+  'output/tesla_stores_in_osm.geojson',
+);
+const outputNotInOsmFilePath = path.join(
+  __dirname,
+  'output/tesla_stores_not_in_osm.geojson',
+);
 
 const scrapedData = JSON.parse(fs.readFileSync(inputScrapFilePath));
 const osmData = JSON.parse(fs.readFileSync(inputOverpassFilePath));
@@ -40,3 +48,47 @@ console.log(
   storesProbablyInOsm.length + storesProbablyNotInOsm.length,
 );
 console.log('scraped', scrapedData.features.length);
+
+const hasError = false;
+
+const geojsonProbablyInOsm = {
+  type: 'FeatureCollection',
+  name: 'Tesla Stores already in OSM',
+  features: storesProbablyInOsm,
+};
+
+fs.writeFile(
+  outputInOsmFilePath,
+  JSON.stringify(geojsonProbablyInOsm, null, 2),
+  (error) => {
+    if (error) {
+      console.error(
+        'Une erreur s\'est produite lors de l\'écriture du fichier "In OSM" :',
+        error,
+      );
+    }
+  },
+);
+
+const geojsonProbablyNotInOsm = {
+  type: 'FeatureCollection',
+  name: 'Tesla Stores NOT in OSM',
+  features: storesProbablyNotInOsm,
+};
+
+fs.writeFile(
+  outputNotInOsmFilePath,
+  JSON.stringify(geojsonProbablyNotInOsm, null, 2),
+  (error) => {
+    if (error) {
+      console.error(
+        'Une erreur s\'est produite lors de l\'écriture du fichier "Not In OSM" :',
+        error,
+      );
+    }
+  },
+);
+
+if (!hasError) {
+  console.log('\x1b[32m \n\n✔ Terminé\n\n \x1b[0m');
+}
